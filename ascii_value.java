4b554c8987447f414015eebd4e5d33510bc8d0ed
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2021-03-22 21:16:07 +0530

    Add files via upload

---
+++ ascii_value.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ascii_value {
+    public static void main(String[] args) {
+        System.out.println("Print Ascii Value of any Number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the any character here");
+        char a = sc.next().charAt(0);
+        int a1 = a;
+        System.out.println(a1);
+
+
+    }
+}
